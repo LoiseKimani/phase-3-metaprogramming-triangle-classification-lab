a09f1c19f81f450baef67789957d67595eae7519
Loise <loise.kimani@student.moringaschool.com>
2023-02-20 02:36:12 +0300

    Done

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
 class Triangle
-  # write code here
+  class TriangleError < StandardError
+  end
+
+  attr_reader :side1, :side2, :side3
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+
+    validate_triangle
+  end
+
+  def kind
+    if side1 == side2 && side2 == side3
+      :equilateral
+    elsif side1 == side2 || side2 == side3 || side1 == side3
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  private
+
+  def validate_triangle
+    raise TriangleError, "Triangles must have positive sides." unless side1.positive? && side2.positive? && side3.positive?
+    raise TriangleError, "Triangles must have a non-zero area." unless side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1
+  end
 end
